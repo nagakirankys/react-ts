ba96cabdf55b8adbf9b4392fc31c91e7614fc9a6
nagakirankys <109279733+nagakirankys@users.noreply.github.com>
2022-09-19 08:31:37 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+import './test.scss';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <h2>Test SCSS</h2>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-vwmcv2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-vwmcv2)
